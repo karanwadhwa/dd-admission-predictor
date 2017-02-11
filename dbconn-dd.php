da2cd1c60fd0c230b1d176608401c7b92a714623
Karan Wadhwa <wadhwa.karan96@gmail.com>
2017-02-11 16:18:01 +0530

    Insignificant commit. moving things around, made a database connection file for db-dd

---
+++ dbconn-dd.php
@@ -0,0 +1,8 @@
+<?php
+
+$conn = mysqli_connect('localhost', 'root', '', 'dd');
+
+if(!$conn){
+ die('Connection Failed: ' . mysqli_connect_error());
+}
+echo 'Connection Successful.' . "<br>";
---
+++ login.php
@@ -0,0 +1,8 @@
+<?php
+include 'dd-admission-predictor/conn-dd.php';
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+echo $username . "<br>";
+echo $password . "<br>";
+ ?>
